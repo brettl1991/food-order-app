1b13907b7503675cc08181fe2b06c2e096a36c67
brettl1991 <brettl1991@gmail.com>
2023-05-09 12:35:12 +0100

    feat: add Meals component with data and style

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,65 @@
+import React from "react";
+import styled from "styled-components";
+
+const DUMMY_MEALS = [
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
+
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  return (
+    <SectionStyle>
+      <MealsStyle>{mealsList}</MealsStyle>
+    </SectionStyle>
+  );
+};
+export default AvailableMeals;
+
+const SectionStyle = styled.section`
+  max-width: 60rem;
+  width: 90%;
+  margin: 2rem auto;
+  animation: meals-appear 1s ease-out forwards;
+`;
+
+const MealsStyle = styled.ul`
+  max-width: 60rem;
+  width: 90%;
+  margin: 2rem auto;
+  animation: meals-appear 1s ease-out forwards;
+
+  @keyframes meals-appear {
+    from {
+      opacity: 0;
+      transform: translateY(3rem);
+    }
+
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+`;
