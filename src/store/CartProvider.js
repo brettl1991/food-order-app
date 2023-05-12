dd6e4dc8b248cd12ae195e1187e4beee23f56fe7
brettl1991 <brettl1991@gmail.com>
2023-05-12 11:18:55 +0100

    fest:change logic to add items to the cart considering if items already added to the cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,5 +1,6 @@
 import React, { useReducer } from "react";
 import CartContext from "./cart-context";
+import { act } from "react-dom/test-utils";
 
 const defaultCartState = {
   items: [],
@@ -8,9 +9,29 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    //items that already part of the cart item array
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    //when item added first time to cart array
+    else {
+      updatedItems = state.items.concat(action.item);
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
